c55e31190386757387e4ae9819303d7496e1bccd
Nick Weisser <nick@openstream.ch>
2015-02-04 13:52:58 +0100

    Shell script for updating

---
+++ update-from-de-de.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cp de_DE/wp-dev-admin-de.po de_CH/wp-dev-admin-de-ch.po 
+cp de_DE/wp-dev-cc-de.po de_CH/wp-dev-cc-de-ch.po 
+cp de_DE/wp-dev-de.po de_CH/wp-dev-de-ch.po 
+cp de_DE/wp-dev-twentyeleven-de.po de_CH/wp-dev-twentyeleven-de-ch.po
+cp de_DE/wp-dev-twentyfifteen-de.po de_CH/wp-dev-twentyfifteen-de-ch.po
+cp de_DE/wp-dev-twentyfourteen-de.po de_CH/wp-dev-twentyfourteen-de-ch.po
+cp de_DE/wp-dev-twentyten-de.po de_CH/wp-dev-twentyten-de-ch.po
+cp de_DE/wp-dev-twentythirteen-de.po de_CH/wp-dev-twentythirteen-de-ch.po
+cp de_DE/wp-dev-twentytwelve-de.po de_CH/wp-dev-twentytwelve-de-ch.po
