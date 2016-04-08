5a22113dc1bbf4b6d39f8ead5798a957266279ff
Nick Weisser <nick@openstream.ch>
2016-04-08 08:17:24 +0200

    Update script for de_CH informal

---
+++ update-from-de-de-informal.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# export/download latest de_DE locates from dev
+curl -o de_DE-informal/wp-dev-de.po https://translate.wordpress.org/projects/wp/dev/de/default/export-translations
+curl -o de_DE-informal/wp-dev-cc-de.po https://translate.wordpress.org/projects/wp/dev/cc/de/default/export-translations
+curl -o de_DE-informal/wp-dev-admin-de.po https://translate.wordpress.org/projects/wp/dev/admin/de/default/export-translations
+curl -o de_DE-informal/wp-dev-admin-network-de.po https://translate.wordpress.org/projects/wp/dev/admin/network/de/default/export-translations
+curl -o de_DE-informal/wp-dev-twentyfifteen-de.po https://translate.wordpress.org/projects/wp/dev/twentyfifteen/de/default/export-translations
+curl -o de_DE-informal/wp-dev-twentyfourteen-de.po https://translate.wordpress.org/projects/wp/dev/twentyfourteen/de/default/export-translations
+
+# copy from de_DE to de_CH
+cp de_DE-informal/wp-dev-de.po de_CH-informal/wp-dev-de-ch.po 
+cp de_DE-informal/wp-dev-cc-de.po de_CH-informal/wp-dev-cc-de-ch.po 
+cp de_DE-informal/wp-dev-admin-de.po de_CH-informal/wp-dev-admin-de-ch.po 
+cp de_DE-informal/wp-dev-admin-network-de.po de_CH-informal/wp-dev-admin-network-de-ch.po
+cp de_DE-informal/wp-dev-twentyfifteen-de.po de_CH-informal/wp-dev-twentyfifteen-de-ch.po
+cp de_DE-informal/wp-dev-twentyfourteen-de.po de_CH-informal/wp-dev-twentyfourteen-de-ch.po
+cp de_DE-informal/wp-dev-twentythirteen-de.po de_CH-informal/wp-dev-twentythirteen-de-ch.po
