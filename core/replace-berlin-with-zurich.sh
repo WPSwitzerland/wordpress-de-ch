6354c3cd368aa449a8cc95f6fd78b39ab830c53f
Nick Weisser <nick@openstream.ch>
2017-11-16 12:38:25 +0100

    Replace Berlin with Zürich

--- core/replace-berlin-with-zurich.sh
+++ core/replace-berlin-with-zurich.sh
@@ -2,6 +2,6 @@
 find de_CH/wp-dev-admin-de-ch.po -type f -exec sed -i '' 's/Berlin/Zürich/g' {} +
 find de_CH/wp-dev-de-ch.po -type f -exec sed -i '' 's/Berlin; Köln; München/Bern; Zürich; Genf/g' {} +
 find de_CH/wp-dev-de-ch.po -type f -exec sed -i '' 's/Berlin/Zürich/g' {} +
-find de_CH-informal/wp-dev-admin-de-ch.po -type f -exec sed -i '' 's/Berlin/Zürich/g' {} +
+find de_CH-informal/wp-dev-admin-de-ch.po -type f -exec sed -i '' 's/Berlin/Zurich/g' {} +
 find de_CH-informal/wp-dev-de-ch.po -type f -exec sed -i '' 's/Berlin; Köln; München/Bern; Zürich; Genf/g' {} +
-find de_CH-informal/wp-dev-de-ch.po -type f -exec sed -i '' 's/Berlin/Zürich/g' {} +
+find de_CH-informal/wp-dev-de-ch.po -type f -exec sed -i '' 's/Berlin/Zurich/g' {} +
