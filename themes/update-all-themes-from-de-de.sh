e6d8c866b747aaa55ec781ea7f6854cb6853f68d
Nick Weisser <nick@openstream.ch>
2018-12-06 12:52:42 +0100

    Add Twenty Nineteen theme to update script

--- themes/update-all-themes-from-de-de.sh
+++ themes/update-all-themes-from-de-de.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # export/download latest de_DE locale translations from themes
 curl -o de_DE/wp-themes-twentyseventeen-de.po https://translate.wordpress.org/projects/wp-themes/twentyseventeen/de/formal/export-translations
+curl -o de_DE/wp-themes-twentynineteen-de.po https://translate.wordpress.org/projects/wp-themes/twentynineteen/de/formal/export-translations
 
 # copy from de_DE to de_CH
 cp de_DE/wp-themes-twentyseventeen-de.po de_CH/wp-themes-twentyseventeen-de-ch.po
+cp de_DE/wp-themes-twentynineteen-de.po de_CH/wp-themes-twentynineteen-de-ch.po
