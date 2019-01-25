0d6a4ebfb3e3d46a871c68b24bc2a3b62a9aed46
Nick Weisser <nick@openstream.ch>
2019-01-25 14:50:14 +0100

    Add Classic Editor plugin to update script

--- plugins/update-all-plugins-from-de-de-informal.sh
+++ plugins/update-all-plugins-from-de-de-informal.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # export/download latest de_DE locale translations from dev
-curl -o woocommerce/de_DE-informal/wp-plugins-woocommerce-dev-de.po https://translate.wordpress.org/projects/wp-plugins/woocommerce/dev/de/default/export-translations
+#curl -o woocommerce/de_DE-informal/wp-plugins-woocommerce-dev-de.po https://translate.wordpress.org/projects/wp-plugins/woocommerce/dev/de/default/export-translations
+curl -o classic-editor/de_DE-informal/wp-plugins-classic-editor-dev-de.po https://translate.wordpress.org/projects/wp-plugins/classic-editor/dev/de/default/export-translations
 
 # copy from de_DE to de_CH
-cp woocommerce/de_DE-informal/wp-plugins-woocommerce-dev-de.po woocommerce/de_CH-informal/wp-plugins-woocommerce-dev-de-ch.po
+#cp woocommerce/de_DE-informal/wp-plugins-woocommerce-dev-de.po woocommerce/de_CH-informal/wp-plugins-woocommerce-dev-de-ch.po
+cp classic-editor/de_DE-informal/wp-plugins-classic-editor-dev-de.po classic-editor/de_CH-informal/wp-plugins-classic-editor-dev-de-ch.po 
--- plugins/update-all-plugins-from-de-de.sh
+++ plugins/update-all-plugins-from-de-de.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # export/download latest de_DE locale translations from dev
-curl -o woocommerce/de_DE/wp-plugins-woocommerce-dev-de.po https://translate.wordpress.org/projects/wp-plugins/woocommerce/dev/de/formal/export-translations
+#curl -o woocommerce/de_DE/wp-plugins-woocommerce-dev-de.po https://translate.wordpress.org/projects/wp-plugins/woocommerce/dev/de/formal/export-translations
+curl -o classic-editor/de_DE/wp-plugins-classic-editor-dev-de.po https://translate.wordpress.org/projects/wp-plugins/classic-editor/dev/de/formal/export-translations
 
 # copy from de_DE to de_CH
-cp woocommerce/de_DE/wp-plugins-woocommerce-dev-de.po woocommerce/de_CH/wp-plugins-woocommerce-dev-de-ch.po
+#cp woocommerce/de_DE/wp-plugins-woocommerce-dev-de.po woocommerce/de_CH/wp-plugins-woocommerce-dev-de-ch.po
+cp classic-editor/de_DE/wp-plugins-classic-editor-dev-de.po classic-editor/de_CH/wp-plugins-classic-editor-dev-de-ch.po
