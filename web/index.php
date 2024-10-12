6011fa798adb40ce9be3e2478a36924215bfa97a
Sandro <sandro@openstream.ch>
2024-10-12 22:02:09 +0200

    load bootstrap local and add via composer

--- web/index.php
+++ web/index.php
@@ -147,7 +147,7 @@ if ( ! empty( $_POST ) ) {
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="/vendor/twbs/bootstrap/dist/css/bootstrap.min.css" crossorigin="anonymous">
 
 	<title>PO Converter for de_DE 2 de_CH</title>
 	<style>
